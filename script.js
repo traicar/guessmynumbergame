dac8ae0d8a50a4aebe4655541a4906ec889ada4f
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2022-01-06 10:31:00 -0600

    guess logic dom

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 'use strict';
 
+/*DOM Manipulation practice
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value;
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+  }
+});
+
